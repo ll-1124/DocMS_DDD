ac2ee9f63eb4d9c3d77d267e039e7590d29af493
ll <luonglv@hisoft.vn>
2025-03-12 14:56:50 +0700

    update domain layer

--- Domain/DocumentManagements/Folders/Folders.cs
+++ Domain/DocumentManagements/Folders/Folders.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Domain.Folders
+namespace Domain.Documents.Folders
 {
     public class Folders : AggregateRoot<FolderId>
     {
